2fe480cfbc0ee886ba2caa4ba99380a1cb4a69d7
Arslankb <arslankhanry@gmail.com>
2023-09-01 11:59:51 +0500

    First Commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { User } from './user';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,12 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'td-Form';
+
+  // topics = ['Angular', 'React', 'Vue'];
+
+  // userModel = new User('Rob', 'rob@gmail.com', 55555555555, '', 'morning', true);
+
+  submit(login: any){
+    console.log("Form Submitted!", login)
+  }
 }
